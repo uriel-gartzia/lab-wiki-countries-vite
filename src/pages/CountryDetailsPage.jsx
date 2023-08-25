9c8a704024820276c5548c38c8a25802a99d7e37
Uriel Gartzia <ugartziasola@gmail.com>
2023-08-25 22:32:35 +0200

    son las 22:30 de un viernes pero qué ilusión me ha hecho terminar este lab y cuánto he aprendido

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,8 +1,61 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
 function CountryDetails() {
 
-return(
+     const [ countryInfo, setCountryInfo ] = useState(null)
 
+     const params = useParams()
+     console.log('info del pais', params)
+
+     useEffect(() => {
+          getData()
+     }, [params.countryId])
+     
+
+     const getData = async () => {
+          try {
+               const response = await axios.get(`https://ih-countries-api.herokuapp.com/countries/${params.countryId}`)
+               console.log('aquí la respuesta de la llamada', response)
+
+               setCountryInfo(response.data)
+               console.log('valor del estado después de modificarlo', countryInfo.alpha2Code)
+          } catch (error) {
+               console.log(error)
+          }
+     }
+
+     if (countryInfo === null) {
+          return <h3>Loading...</h3>
+     }
+
+return(
+<>
      <h3>Country Details</h3>
+
+     <img src={`https://flagpedia.net/data/flags/icon/72x54/${countryInfo.alpha2Code.toLowerCase()}.png`} width={100} alt="flag" />
+     <h2>{countryInfo.name.official}</h2>
+     <p>Capital {countryInfo.capital}</p>
+     <hr />
+     <p>Area {countryInfo.area} km2</p>
+     <p>Borders</p>
+     {countryInfo.borders.map((eachBorder) => {
+          return (
+
+               <Link to={`/${eachBorder}`}>
+                    <li>{eachBorder}</li>
+               </Link>
+
+               
+          )
+     })}
+
+
+
+</>
+     
+
 )
    
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,51 +2,57 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 function HomePage() {
+  const [countriesList, setCountriesList] = useState(null);
 
-    const [ countryInfo, setCountryInfo ] = useState(null)
+  useEffect(() => {
+    getData();
+  }, []);
 
-    useEffect(() => {
-        getData()
-    }, [])
-
-    const getData = async () => {
-        try {
-            const response = await axios.get('https://ih-countries-api.herokuapp.com/countries')
-            console.log('aquí debería recibir la lista de países procedente de la API', response)
-            setCountryInfo(response.data)
-            // console.log('Aquí deberían llegar las banderas', countryInfo.alpha2Code)
-        } catch (error) {
-            console.log(error)
-        }
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "https://ih-countries-api.herokuapp.com/countries"
+      );
+      console.log(
+        "aquí debería recibir la lista de países procedente de la API",
+        response
+      );
+      setCountriesList(response.data);
+      // console.log('Aquí deberían llegar las banderas', countryInfo.alpha2Code)
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    if (countryInfo === null) {
-        return <h3>... buscando</h3>
-      }
-     
-    return (
-<>
-    <div className='container' style={{maxHeight: '90vh'}} overflow= {{scroll}}>
-        <h1 style={{fontSize: '24px'}}>WikiCountries: Your Guide to the World</h1>
-    </div>
+  if (countriesList === null) {
+    return <h3>Searching...</h3>;
+  }
 
-    {countryInfo.map((eachCountry, i) => {
+  return (
+    <>
+      <div
+        className="container"
+        style={{ maxHeight: "90vh" }}
+        overflow={{ scroll }}
+      >
+        <h1 style={{ fontSize: "24px" }}>
+          WikiCountries: Your Guide to the World
+        </h1>
+      </div>
+
+      {countriesList.map((eachCountry, i) => {
         return (
-    <div key={i} className="list-group">
-        <Link to={`${eachCountry.alpha3Code}`}>
-            <img src={`https://flagpedia.net/data/flags/icon/72x54/${eachCountry.alpha2Code.toLowerCase()}.png`} width={50} />
-            <p className="list-group-item list-group-item-action">{eachCountry.name.official}</p>
-        </Link> 
-    </div> 
-        )
-    })}
-   
-</>
-    
-   
-)}
+          <div key={i} className="list-group">
+            <Link to={`${eachCountry.alpha3Code}`}>
+              <img src={`https://flagpedia.net/data/flags/icon/72x54/${eachCountry.alpha2Code.toLowerCase()}.png`} width={50}/>
+              <p className="list-group-item list-group-item-action">{eachCountry.name.official}</p>
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
+}
 
 export default HomePage;
